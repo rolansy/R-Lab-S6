afc420e5982851d408f1df3ed28f27b7eca25dd1
rolansy <ronalshoeygeorge@gmail.com>
2025-01-29 12:12:11 +0530

    Improve output formatting for data summary in R script

--- 1.r
+++ 1.r
@@ -1,24 +1,23 @@
 a<-c(10,15,20,25,30,35,40,45,50)
 b<-c(5,10,15,20,25,30,35,40,45)
-cat('Data A : ',a)
-cat('Mean : ',mean(a))
-cat('Median : ',median(a))
-cat('Summary : ',summary(a))
+cat('\nData A : ',a)
+cat('\nMean : ',mean(a))
+cat('\nMedian : ',median(a))
+cat('\nSummary : ',summary(a))
 s1<-'con'
 s2<-'cat'
-cat('Min : ',min(a))
-cat('Max : ',max(a))
-cat('1st Qaurtile : ',quantile(a,0.25))
-cat('3st Qaurtile : ',quantile(a,0.75))
-cat('Summary : ',summary(a))
-cat('Concat of ',s1,'and',s2,end="")
-cat(s1,s2,sep='')
-cat('Variance : ',var(a))
+cat('\nMin : ',min(a))
+cat('\nMax : ',max(a))
+cat('\n1st Qaurtile : ',quantile(a,0.25))
+cat('\n3st Qaurtile : ',quantile(a,0.75))
+cat('\nSummary : ',summary(a))
+
+cat('\nVariance : ',var(a))
 boxplot(a,main='Boxplot of A',col='cyan')
 
-cat('Data B : ',b)
-cat('Covariance of A and B : ',cov(a,b))
-cat('Correlation of A and B : ',cor(a,b))
+cat('\nData B : ',b)
+cat('\nCovariance of A and B : ',cov(a,b))
+cat('\nCorrelation of A and B : ',cor(a,b))
 
 
 
