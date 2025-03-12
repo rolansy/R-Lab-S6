7021f01d020be14bbdc7276be6ff8b818ef2deef
rolansy <ronalshoeygeorge@gmail.com>
2025-03-12 11:56:05 +0530

    Add initial setup for decision tree model with required libraries

---
+++ decision tree/dec_id3_5.r
@@ -0,0 +1,11 @@
+#Installing libraries
+install.packages('rpart')
+install.packages('caret')
+install.packages('rpart.plot')
+install.packages('rattle')
+
+#Loading libraries
+library(rpart,quietly = TRUE)
+library(caret,quietly = TRUE)
+library(rpart.plot,quietly = TRUE)
+library(rattle)
