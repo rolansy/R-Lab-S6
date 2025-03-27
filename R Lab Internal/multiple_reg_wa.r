7f5919e66e60cd8d6169018be9e5f3dcb55489e6
rolansy <ronalshoeygeorge@gmail.com>
2025-03-27 12:33:46 +0530

    Add prediction and plotting functionality to multiple linear regression model in R

--- R Lab Internal/multiple_reg_wa.r
+++ R Lab Internal/multiple_reg_wa.r
@@ -7,5 +7,14 @@ model
 summary(model)
 
 ah<-as.integer(readline(prompt="Enter Height : "))
-aa<-as.integer(readline(prompt="Enter Age : "))
+
+aa<-as.integer(readline(prompt="Enter Age :"))
+
+a<-data.frame(Height=ah,Age=aa)
+
+result<-predict(model,a)
+result
+
+plot(data)
+
 
