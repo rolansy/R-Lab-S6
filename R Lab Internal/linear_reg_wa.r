5487ce696bb2e23848f5b38f4a8a37608df7871e
rolansy <ronalshoeygeorge@gmail.com>
2025-03-27 12:33:26 +0530

    Add height prediction functionality to linear regression model in R with visualization

--- R Lab Internal/linear_reg_wa.r
+++ R Lab Internal/linear_reg_wa.r
@@ -2,3 +2,16 @@ data<-read.csv("linear_reg.csv")
 data
 
 model<-lm(Weight ~ Height,data=data)
+summary(model)
+model_sum<-summary(model)
+model_sum_coef<-model_sum$coefficients
+
+a<-as.integer(readline(prompt = "Enter height : "))
+a<-data.frame(Height=a)
+a
+result<-predict(model,a)
+result
+
+plot(data$Height,data$Weight,col="red",main="Linear Regression",cex=1.3,pch='*',)
+abline(model,col="blue",lty=1)
+
